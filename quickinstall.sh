c2ebe06390801eb0899240dfbd59182c208803d7
sko9370 <sko9370@ymail.com>
2020-05-05 22:35:12 -0400

    save

---
+++ quickinstall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# set privs for shared folder
+sudo usermod -aG vboxsf $(whoami)
+sudo apt-get install -y gcc make perl vim git curl tmux python3-pip xsel nmap netcat
+
+cp .tmux.conf .vimrc .bashrc ..
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/pwndbg/pwndbg
+./$HOME/pwndbg/setup.py
