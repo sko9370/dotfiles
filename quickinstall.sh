57b306f188e23864b718ecf17df4ed3d9682defb
sko9370 <sko9370@ymail.com>
2020-06-25 13:38:50 -0400

    save

--- quickinstall.sh
+++ quickinstall.sh
@@ -4,8 +4,8 @@
 sudo usermod -aG vboxsf $(whoami)
 sudo apt-get install -y gcc make perl vim git curl tmux python3-pip xsel nmap netcat
 
-cp .tmux.conf .vimrc .bashrc ..
+cp .tmux.conf .vimrc .bashrc ~/
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/pwndbg/pwndbg
-./$HOME/pwndbg/setup.py
+#git clone https://github.com/pwndbg/pwndbg
+#./$HOME/pwndbg/setup.py
