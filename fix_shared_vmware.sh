f2abbea550581b3f2bf5ea58885f5ce6ed2cad96
sko9370 <sko9370@ymail.com>
2022-04-28 09:14:05 -0400

    add vmware shared folders fix

---
+++ fix_shared_vmware.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo apt-get install open-vm-tools
+
+sudo cp mnt-hgfs.mount /etc/systemd/system/mnt-hgfs.mount/
+
+sudo cp open-vm-tools.conf /etc/modules-load.d/
+
+sudo systemctl enable mnt-hgfs.mount
+
+sudo modprobe -v fuse
+
+sudo systemctl start mnt-hgfs.mount
--- quickinstall.sh
+++ quickinstall.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-# set privs for shared folder
-sudo usermod -aG vboxsf $(whoami)
 sudo apt-get install -y gcc make perl vim git curl tmux python3-pip xsel nmap netcat
 
 cp .tmux.conf .vimrc .bashrc ~/
